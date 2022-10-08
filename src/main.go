1300f988b3820a1292bdf707ab41934ca4f0019e
Kemal Yildirim <kemalyildirm@gmail.com>
2022-10-08 20:46:16 +0300

    viper config file operations

--- src/main.go
+++ src/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/kemalyildirim/sshfam/cmd/ssh"
+import (
+	"log"
+	"os"
+
+	"github.com/kemalyildirim/sshfam/cmd/ssh"
+	"github.com/spf13/viper"
+)
+
+func initConfig() {
+	viper.SetConfigName(ssh.ROOT_CMD)
+	viper.SetConfigType("properties")
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	viper.AddConfigPath(homeDir)
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Println("viper: config not found")
+		} else {
+			// Config file was found but another error was produced
+			log.Println("viper: config found with error")
+			log.Fatal(err)
+		}
+	}
+
+	// Config file found and successfully parsed
+}
 
 func main() {
+	initConfig()
 	ssh.Execute()
 }
