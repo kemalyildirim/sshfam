c2b38244f490a00cd73cf793cfc2e05d1f33c5da
Kemal Yildirim <kemalyildirm@gmail.com>
2022-10-15 00:58:48 +0300

    fix subcommands

--- src/cmd/ssh/connect.go
+++ src/cmd/ssh/connect.go
@@ -3,17 +3,15 @@ package ssh
 import (
 	"fmt"
 
-	"github.com/kemalyildirim/sshfam/src/commons"
 	"github.com/spf13/cobra"
 )
 
 var connectCli = &cobra.Command{
-	Use:   commons.ROOT_CMD + " connect [user]@[remote_ip]",
+	Use:   "connect [user]@[remote_ip]",
 	Short: "Connect to a server via SSH. Usage: sshfam connect ",
 	Long:  "Connect to a server via SSH. Save crendentials on successful login. If the credentials change override them.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("connect")
-
 		fmt.Println("args: ", args)
 	},
 }
--- src/cmd/ssh/list.go
+++ src/cmd/ssh/list.go
@@ -3,16 +3,14 @@ package ssh
 import (
 	"fmt"
 
-	"github.com/kemalyildirim/sshfam/src/commons"
 	"github.com/spf13/cobra"
 )
 
 var listCli = &cobra.Command{
-	Use:   commons.ROOT_CMD + " list",
+	Use:   "list",
 	Short: "List saved sessions. Usage: sshfam list",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list")
-
 		fmt.Println("args: ", args)
 	},
 }
